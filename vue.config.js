53a6ca22479bf5a86564b62937203d7a7822456b
Eronne <erwann.letue@gmail.com>
2020-04-17 13:14:44 +0200

    chore(build): Add webpack-bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+
 module.exports = {
+  configureWebpack: {
+    plugins: [new BundleAnalyzerPlugin()]
+  },
   css: { extract: false }
 };
