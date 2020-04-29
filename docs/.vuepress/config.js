4a46fb28dccdce935c610de003accf3df46f5430
Eronne <erwann.letue@gmail.com>
2020-04-29 12:06:15 +0200

    docs(vuepress): Add configuration

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  themeConfig: {
+    logo: '/assets/img/logo.png',
+    nav: [
+      { text: 'Guide', link: '/guide/' },
+    ],
+    sidebar: 'auto',
+    repo: 'FashionData/maestro',
+    smoothScroll: true,
+  }
+}
