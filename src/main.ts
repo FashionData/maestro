eee7b7730e251d07039d1190b03d490400cbeee1
Eronne <erwann.letue@gmail.com>
2020-05-03 22:37:43 +0200

    chore(views): Add style + integrated profile page

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ import { HOME } from "@/constants/router/routes";
 import { authMiddleware } from "@/router/middleware";
 import HomeView from "@/views/placeholders/HomeView.vue";
 
+// TODO: remove when style will be globally exposed
 import "@/styles/app.scss"
 
 // Define typescript interfaces for autoinstaller
