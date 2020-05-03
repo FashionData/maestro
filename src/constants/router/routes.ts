c94fc09b85f5a34fa0e8eac9b5dbed94e7c6f12e
Eronne <erwann.letue@gmail.com>
2020-05-03 22:35:39 +0200

    chore(views): Renamed user to profile view

--- src/constants/router/routes.ts
+++ src/constants/router/routes.ts
@@ -2,5 +2,5 @@ export const HOME = { name: "home", path: "/" };
 export const LOGIN = { name: "login", path: "/login" };
 export const LOGOUT = { name: "logout", path: "/logout" };
 export const USERS = { name: "users", path: "/users" };
-export const USER = { name: "user", path: "/user" };
+export const PROFILE = { name: "profile", path: "/profile" };
 export const NOT_FOUND = { name: "not-found", path: "*" };
