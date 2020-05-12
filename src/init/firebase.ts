329896ba0f5acd9f1956cebd335b364d1a06c640
Omar Jbara <omar.jbara2@gmail.com>
2020-05-12 18:30:17 +0200

    feat(query): can now provide query with httpsCallable

--- src/init/firebase.ts
+++ src/init/firebase.ts
@@ -6,9 +6,22 @@ import { REGION } from "@/constants/firebase";
 export const configureFirebase = (Vue: VueConstructor, firebase: AnyObject) => {
   Vue.prototype.$firebase = firebase;
 
-  Vue.prototype.$httpsCallableFunction = (name: string, data: AnyObject = {}) => {
+  Vue.prototype.$httpsCallableFunction = (
+    name: string,
+    query: { [key: string]: string | number } = {},
+    data: AnyObject = {}
+  ) => {
+    const parsedQuery = Object.entries(query)
+      .reduce((acc, entry) => {
+        return acc + entry[0] + "=" + entry[1] + "&";
+      }, "")
+      .slice(0, -1);
+    const hasQuery = Object.keys(parsedQuery).length > 0;
     return new Promise((resolve, reject) => {
-      const callableFunction = firebase.app().functions(REGION).httpsCallable(name);
+      const callableFunction = firebase
+        .app()
+        .functions(REGION)
+        .httpsCallable(`${name}${hasQuery ? "?" + parsedQuery : ""}`);
 
       callableFunction(data)
         .then(resolve)
@@ -16,7 +29,7 @@ export const configureFirebase = (Vue: VueConstructor, firebase: AnyObject) => {
     });
   };
 
-  firebase.auth().onAuthStateChanged((user : User) => {
+  firebase.auth().onAuthStateChanged((user: User) => {
     if (user) {
       userStore.state.user = user;
       userStore.state.isAuthenticated = true;
