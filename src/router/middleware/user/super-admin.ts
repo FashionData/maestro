7f4bf008a32a5834d79320a0a080727b540d095b
Omar Jbara <omar.jbara2@gmail.com>
2020-05-31 06:51:03 +0200

    feat(router): added superAdminMiddleware

---
+++ src/router/middleware/user/super-admin.ts
@@ -0,0 +1,16 @@
+import { Context, Middleware, MiddlewarePipeline } from "@/types/router";
+import { HOME } from "@/constants/router/routes";
+import { Roles } from "@/constants";
+
+export const superAdminMiddleware: Middleware = (
+  { next, userStore }: Context,
+  nextPipeline: ReturnType<MiddlewarePipeline>
+) => {
+  if (
+    userStore.state.user &&
+    userStore.state.user.role.code >= Roles.SuperAdmin
+  ) {
+    return nextPipeline();
+  }
+  return next(HOME);
+};
