9577633abed581e643dcc55ac81674fcd20980b7
Omar Jbara <omar.jbara2@gmail.com>
2020-06-17 22:02:56 +0200

    feat(httpsCallaback): injected currentAccount

--- src/init/store.ts
+++ src/init/store.ts
@@ -2,8 +2,10 @@ import { AnyObject } from "@/types";
 import { userStore } from "@/store/user";
 import { VueConstructor } from "vue";
 
+export const MODULE_NAME = "m-user";
+
 export const configureStore = (Vue: VueConstructor, appStore: AnyObject) => {
-  appStore.registerModule("m-user", userStore);
+  appStore.registerModule(MODULE_NAME, userStore);
   Vue.prototype.$isGranted = function(role: number) {
     return this.$store.getters.user?.role?.code >= role;
   };
