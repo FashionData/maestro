c1380fd64cc5df4fe13aa340b88873a6825c35a4
Eronne <erwann.letue@gmail.com>
2021-06-01 10:46:48 +0200

    chore(m-virtual-displays): Improve component UI

--- src/components/m-virtual-displays/m-virtual-displays.vue
+++ src/components/m-virtual-displays/m-virtual-displays.vue
@@ -1,13 +1,17 @@
 <template>
-  <el-card shadow="none" class="m-virtual-displays">
-    <div class="d-flex justify-space-between mb-13">
+  <div class="m-virtual-displays">
+    <el-card
+      v-if="hasFilters || hasSeveralDisplays"
+      shadow="none"
+      body-style="display: flex; justify-content: space-between; align-content: center;"
+    >
       <div class="filters d-flex">
         <slot name="filters" />
       </div>
 
       <div class="actions d-flex align-center">
         <i v-if="hasFilters" class="ri-restart-line" @click="emitResetFilters" />
-        <template v-if="displays.length > 1">
+        <template v-if="hasSeveralDisplays">
           <i
             v-for="display in displays"
             :key="display.type"
@@ -16,93 +20,35 @@
           />
         </template>
       </div>
-    </div>
+    </el-card>
 
-    <div v-if="items.length > 0" v-loading="loading">
-      <div v-if="displayType === 'table'" class="table-display">
-        <div class="headers mb-6">
-          <div
-            v-for="(header, index) in headers"
-            :key="header.value"
-            class="text--info text--uppercase"
-            :style="{ width: header.width ? `${header.width}px` : getColumnWidth(index) }"
-          >
-            {{ header.text }}
-          </div>
-        </div>
-
-        <RecycleScroller
-          :items="items"
-          :item-size="tableItemSize"
-          :key-field="keyField"
-          :style="{ height }"
-          v-slot="{ index, item }"
-        >
-          <div class="row" :style="{ height: `${tableItemSize}px` }">
-            <template v-for="(header, index) in headers" :style="{ width: getColumnWidth(index) }">
-              <div class="column" :class="{ 'text--primary bold': header.textPrimary }" :style="{ width: getColumnWidth(index) }">
-                <slot
-                  v-if="$scopedSlots[`table.item.${header.value}`]"
-                  :name="`table.item.${header.value}`"
-                  v-bind:index="index"
-                  v-bind:header="header"
-                  v-bind:item="item"
-                />
-                <p v-else>
-                  {{ getItemValue(item, header.value) }}
-                </p>
-              </div>
-            </template>
-          </div>
-        </RecycleScroller>
-      </div>
-
-      <div v-if="displayType === 'cards'" class="cards-display">
-        <div class="headers mb-6" :class="{ 'headers--expandable-cards': $scopedSlots['cards.item.extended'] }">
-          <div
-            v-for="(header, index) in headers"
-            :key="header.value"
-            class="text--info text--uppercase"
-            :style="{ width: getColumnWidth(index) }"
-          >
-            {{ header.text }}
-          </div>
-        </div>
-
-        <DynamicScroller
-          :items="items"
-          :min-item-size="cardSize"
-          :key-field="keyField"
-          :style="{ height }"
-          v-slot="{ index, item }"
-        >
-          <DynamicScrollerItem :item="item" active class="pb-10">
-            <el-card
-              body-style="display: flex; align-items: center; padding: 0; height: 100%;"
-              :style="{ height: `${cardSize}px` }"
+    <el-card shadow="none" v-loading="loading">
+      <template v-if="items.length > 0">
+        <div v-if="displayType === 'table'" class="table-display">
+          <div v-if="headersHaveText" class="headers mb-6">
+            <div
+              v-for="(header, index) in headers"
+              :key="header.value"
+              class="text--info text--uppercase"
+              :style="{ width: header.width ? `${header.width}px` : getColumnWidth(index) }"
             >
-              <div>
-                <el-button
-                  v-if="$scopedSlots['cards.item.extended']"
-                  :type="extentedCardIndex === index ? 'primary' : 'default'"
-                  size="small"
-                  icon="ri-arrow-right-s-line ri-lg"
-                  circle
-                  class="expandable-button p-1"
-                  :class="{ 'expandable-button--active': extentedCardIndex === index }"
-                  @click="toggleCardExtension(index, item)"
-                />
-              </div>
+              {{ header.text }}
+            </div>
+          </div>
 
+          <RecycleScroller
+            :items="items"
+            :item-size="tableItemSize"
+            :key-field="keyField"
+            :style="{ height }"
+            v-slot="{ index, item }"
+          >
+            <div class="row" :style="{ height: `${tableItemSize}px` }">
               <template v-for="(header, index) in headers" :style="{ width: getColumnWidth(index) }">
-                <div
-                  class="column"
-                  :class="{ 'text--primary bold': header.textPrimary }"
-                  :style="{ width: getColumnWidth(index) }"
-                >
+                <div class="column" :class="{ 'text--primary bold': header.textPrimary }" :style="{ width: getColumnWidth(index) }">
                   <slot
-                    v-if="$scopedSlots[`cards.item.${header.value}`]"
-                    :name="`cards.item.${header.value}`"
+                    v-if="$scopedSlots[`table.item.${header.value}`]"
+                    :name="`table.item.${header.value}`"
                     v-bind:index="index"
                     v-bind:header="header"
                     v-bind:item="item"
@@ -112,40 +58,100 @@
                   </p>
                 </div>
               </template>
-            </el-card>
-
-            <div class="card-expansion" v-if="extentedCardIndex === index">
-              <slot name="cards.item.extended" v-bind:item="item" />
             </div>
-          </DynamicScrollerItem>
-        </DynamicScroller>
-      </div>
-
-      <RecycleScroller
-        v-if="displayType === 'grid'"
-        :items="items"
-        :item-size="gridItemSize"
-        key-field="id"
-        :style="{ height }"
-        v-slot="{ item }"
-      >
-        <el-card class="card">
-          <pre>{{ item }}</pre>
-          <el-button>CLICK</el-button>
-        </el-card>
-      </RecycleScroller>
-
-      <template v-for="customDisplay in customSlots">
-        <div v-if="customDisplay === `displays.${displayType}`" :key="customDisplay">
-          <slot :name="customDisplay" />
+          </RecycleScroller>
         </div>
-      </template>
-    </div>
 
-    <div v-else>
-      <p>{{ $t('components.m-virtual-displays.no-data') }}</p>
-    </div>
-  </el-card>
+        <div v-if="displayType === 'cards'" class="cards-display">
+          <div class="headers mb-6" :class="{ 'headers--expandable-cards': $scopedSlots['cards.item.extended'] }">
+            <div
+              v-for="(header, index) in headers"
+              :key="header.value"
+              class="text--info text--uppercase"
+              :style="{ width: getColumnWidth(index) }"
+            >
+              {{ header.text }}
+            </div>
+          </div>
+
+          <DynamicScroller
+            :items="items"
+            :min-item-size="cardSize"
+            :key-field="keyField"
+            :style="{ height }"
+            v-slot="{ index, item }"
+          >
+            <DynamicScrollerItem :item="item" active class="pb-10">
+              <el-card
+                body-style="display: flex; align-items: center; padding: 0; height: 100%;"
+                :style="{ height: `${cardSize}px` }"
+              >
+                <div>
+                  <el-button
+                    v-if="$scopedSlots['cards.item.extended']"
+                    :type="extentedCardIndex === index ? 'primary' : 'default'"
+                    size="small"
+                    icon="ri-arrow-right-s-line ri-lg"
+                    circle
+                    class="expandable-button p-1"
+                    :class="{ 'expandable-button--active': extentedCardIndex === index }"
+                    @click="toggleCardExtension(index, item)"
+                  />
+                </div>
+
+                <template v-for="(header, index) in headers" :style="{ width: getColumnWidth(index) }">
+                  <div
+                    class="column"
+                    :class="{ 'text--primary bold': header.textPrimary }"
+                    :style="{ width: getColumnWidth(index) }"
+                  >
+                    <slot
+                      v-if="$scopedSlots[`cards.item.${header.value}`]"
+                      :name="`cards.item.${header.value}`"
+                      v-bind:index="index"
+                      v-bind:header="header"
+                      v-bind:item="item"
+                    />
+                    <p v-else>
+                      {{ getItemValue(item, header.value) }}
+                    </p>
+                  </div>
+                </template>
+              </el-card>
+
+              <div class="card-expansion" v-if="extentedCardIndex === index">
+                <slot name="cards.item.extended" v-bind:item="item" />
+              </div>
+            </DynamicScrollerItem>
+          </DynamicScroller>
+        </div>
+
+        <RecycleScroller
+          v-if="displayType === 'grid'"
+          :items="items"
+          :item-size="gridItemSize"
+          key-field="id"
+          :style="{ height }"
+          v-slot="{ item }"
+        >
+          <el-card class="card">
+            <pre>{{ item }}</pre>
+            <el-button>CLICK</el-button>
+          </el-card>
+        </RecycleScroller>
+
+        <template v-for="customDisplay in customSlots">
+          <div v-if="customDisplay === `displays.${displayType}`" :key="customDisplay">
+            <slot :name="customDisplay" />
+          </div>
+        </template>
+      </template>
+
+      <div v-else :style="{ height }">
+        <p>{{ $t('components.m-virtual-displays.no-data') }}</p>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -209,8 +215,18 @@ export default {
     hasFilters() {
       return this.$slots.filters;
     },
-    currentDisplay() {
-      return this.displays.find(display => display.type === this.displayType);
+    hasSeveralDisplays() {
+      return this.displays.length > 1;
+    },
+    headersHaveText() {
+      let headersHaveText = false;
+      for (let i = 0; i < this.headers.length; i++) {
+        if (this.headers[i].text.length > 0) {
+          headersHaveText = true;
+          break;
+        }
+      }
+      return headersHaveText;
     },
     customSlots() {
       return Object.keys(this.$scopedSlots).filter(slotName => slotName.includes('displays.'));
